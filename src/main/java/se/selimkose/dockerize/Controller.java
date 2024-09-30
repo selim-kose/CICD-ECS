f31348126f014f19ebe94b64da0f2b05e88b8a08
selim <selim-kose@live.com>
2024-09-30 13:41:59 +0200

    second commit

---
+++ src/main/java/se/selimkose/dockerize/Controller.java
@@ -0,0 +1,13 @@
+package se.selimkose.dockerize;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
---
+++ src/main/java/se/selimkose/dockerize/DockerizeApplication.java
@@ -0,0 +1,13 @@
+package se.selimkose.dockerize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeApplication.class, args);
+	}
+
+}
